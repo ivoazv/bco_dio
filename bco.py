d75db65c68541ff7533a5281848265fd067634b7
ivoazv <ivobastos82@gmail.com>
2024-04-14 07:53:45 -0300

    Algumas linhas de código adicionadas completando a primeira parte do exercício.

--- bco.py
+++ bco.py
@@ -1,4 +1,5 @@
-
+from abc import ABC
+from datetime import datetime  
 
 class Cliente:
 	def __init__(self, endereco):
@@ -67,7 +68,6 @@ class Conta:
 
 
 
-
 	def sacar(self, valor):
 		saldo = self._saldo
 		excedeu_limite = valor > self._saldo
@@ -105,5 +105,98 @@ class ContaCorrente(Conta):
 
 
 
+	def sacar(self, valor):
+		numero_saques = len (
+			[transacao for transacao in self.historico.transacoes if transacao["tipo"] == Saque.__name__]
+		)
+
+		excedeu_limite = valor > self.limite
+		excedeu_saques = numero_saques >= self.limite_saques
+
+		if excedeu_limite:
+			print ("O valor excede o limite de saque.")
+		elif excedeu_saques:
+			print ("Número de saques excedido!")
+		else:
+			return super().sacar(valor)
+		
+		return False
+	
+
+
+	def __str__(self):
+		return f"""
+				Agência:\t{self.agencia}
+				C/C:\t{self.numero}
+				Titular:\t{self.cliente.nome}
+		"""
+	
+
+
+
 class Historico():
+	def __init__(self):
+		self._transacoes = []
+
+	@property
+	def get_transacoes(self):
+		return self._transacoes
+
+
 	def adicionar_transacao(transacao):
+		self._transacoes.append(
+			{
+				"tipo" : transacao.__class__.__name__,
+				"valor" : transacao.valor,
+				"data" : datetime.now().strftime("%d-%m-%Y %H:%M:%s"),
+			}
+		)
+
+
+class Transacao(ABC):
+	@property
+	@abstractproperty
+	def valor(self):
+		pass
+
+
+	@abstractclassmethod
+	def registrar(self, conta):
+		pass
+
+
+
+class Saque(Transacao):
+	def __init__(self, valor):
+		self._valor = valor
+
+
+	@property
+	def valor(self):
+		return self._valor
+	
+
+	def registrar(self, conta):
+		sucesso_transacao = conta.sacar(self.valor)
+
+		if sucesso_transacao:
+			conta.historico.adicionar_transacao(self)
+
+
+
+
+class Deposito(Transacao):
+	def __init__(self, valor):
+		self._valor = valor
+
+
+	@property
+	def valor(self):
+		return self._valor
+	
+
+	def registrar(self, conta):
+		sucesso_transacao = conta.depositar(self.valor)
+
+		if sucesso_transacao:
+			conta.historico.adicionar_transacao(self)
